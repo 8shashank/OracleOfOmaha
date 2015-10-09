b6f18ababa22c5445cd727a818f4aed83443b3b0
shashank_sharma_ <8shashank@gmail.com>
2015-10-09 00:57:28 -0500

    Added some empty models

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ stockapi.getStockInfo('AAPL', function(err, response){
     console.log(response);
 });
 
+function User(name, transactions, portfolio, rules){
+    this.name=name;
+    this.transactions=transactions;
+    this.portfolio=portfolio;
+    this.rules= rules|| [];
+}
 
+function Transaction(stock, price, amount, action, timestamp){
+    this.stock=stock;
+    this.price=price;
+    this.amount=amount;
+    this.action=action;
+    this.timestamp=timestamp;
+}
+
+function PortfolioAsset(name, amount){
+    this.name=name;
+    this.amount=amount;
+}
