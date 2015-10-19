6dd42bca49a83bdb3c1bf399b3944a408c6d80b3
shashank_sharma_ <8shashank@gmail.com>
2015-10-18 20:27:46 -0500

    Removed unnecessary stuff from API call

--- stockapi.js
+++ stockapi.js
@@ -3,20 +3,23 @@ var yahooFinance = require('yahoo-finance');
 function getStockInfo(symbol, cb) {
     yahooFinance.snapshot({
         symbol: symbol,
-        //Symbol, Name, Last Trade Price, Ask(realtime), Bid(realtime), Dividend Yield,
+        //Symbol, Name, Last Trade Price
+        // Commented out stuff: Ask(realtime), Bid(realtime), Dividend Yield,
         //P/E Ratio, 50-day Moving Average, 200-day Moving Average,
         //Percent Change From 200-day Moving Average, Percent Change From 50-day Moving Average
-        fields: ['s', 'n', 'l1', 'b2','b3', 'y', 'r', 'm3', 'm4', 'm6', 'm8'],
+        fields: ['s', 'n', 'l1'/*, 'b2','b3', 'y', 'r', 'm3', 'm4', 'm6', 'm8'*/],
     }, cb);
 }
 
 function getMultipleStocksInfo(symbols, cb){
     yahooFinance.snapshot({
         symbols: symbols,
-        //Symbol, Name, Last Trade Price, Ask(realtime), Bid(realtime), Dividend Yield,
+        //Symbol, Name, Last Trade Price
+        
+        // Commented out stuff: Ask(realtime), Bid(realtime), Dividend Yield,
         //P/E Ratio, 50-day Moving Average, 200-day Moving Average,
         //Percent Change From 200-day Moving Average, Percent Change From 50-day Moving Average
-        fields: ['s', 'n', 'l1', 'b2','b3', 'y', 'r', 'm3', 'm4', 'm6', 'm8'],
+        fields: ['s', 'n', 'l1' /*, 'b2','b3', 'y', 'r', 'm3', 'm4', 'm6', 'm8'*/],
     }, cb);
 }
 
